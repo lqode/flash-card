6a11b4fe66915efbaaba5b4a445abd3edae2e671
lqode <125162154+lqode@users.noreply.github.com>
2023-05-01 16:21:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Learn Italian with me
+This is a very useful game where you can work on your skills in learning new words. Currently it is 
+implemented with learning Italian.
+
+It can be easily customized based on whatever skill you want to work on, whether its a new langugae, or 
+computer programming, politics or General Knowledge.
+
+You get 3 seconds to guess the English translation of the Italian word. If you know the word, click on "Green" tick
+button, and it won't quiz you again on that. If you don't know a word, click the "Red" cross button. This will make sure 
+to keep it in your words to learn list.
+
+You can view the words you need working on in words_to_learn.csv.
+
+![](images/img1.png)
+
+![](images/img2.png)
---
+++ src/main.py
@@ -0,0 +1,67 @@
+import random
+from tkinter import *
+import pandas
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+# --------------- Card ----------------
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("data/italian_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="Italian", fill="black")
+    canvas.itemconfig(card_word, text=current_card["Italian"], fill="black")
+    canvas.itemconfig(card_image, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_image, image=card_back_img)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
+
+def is_known():
+    to_learn.remove(current_card)
+    df = pandas.DataFrame(to_learn)
+    df.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
+# --------------- UI Setup ----------------
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_image = canvas.create_image(400, 262, image=card_front_img)
+
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
+wrong_button.grid(row=1, column=0)
+
+right_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_image, highlightthickness=0, command=is_known)  # lambda: [next_card(), is_known()])
+right_button.grid(row=1, column=1)
+
+next_card()
+
+window.mainloop()
